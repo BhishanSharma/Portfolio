b239676ae35943d16ddb630a0eb2549303e97ee1
BhishanSharma <bhishansharma2@gmail.com>
2024-10-29 10:24:02 +0530

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+let el = document.getElementById("work");
+el.onclick = (e) => {
+    window.location.href = "index.html";
+};
+let el2 = document.getElementById("info");
+el2.onclick = (e) => {
+    window.location.href = "info.html";
+};
